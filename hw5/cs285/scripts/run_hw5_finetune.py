05ae826ad479b27fa16ce641e47d17fd696014bf
Vivek Myers <33012196+vivekmyers@users.noreply.github.com>
2023-11-15 01:38:32 -0800

    Update run_hw5_finetune.py

--- hw5/cs285/scripts/run_hw5_finetune.py
+++ hw5/cs285/scripts/run_hw5_finetune.py
@@ -163,7 +163,7 @@ def main():
     args = parser.parse_args()
 
     # create directory for logging
-    logdir_prefix = "hw5_explore_"  # keep for autograder
+    logdir_prefix = "hw5_finetune_"  # keep for autograder
 
     config = make_config(args.config_file)
     logger = make_logger(logdir_prefix, config)
